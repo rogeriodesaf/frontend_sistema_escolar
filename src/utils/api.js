3fc63513d0298de183c74748383420df29ca3ccf
Rogério de Sá <rogeriodesaf@gmail.com>
2023-08-27 05:48:30 -0300

    rotA DE produção

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,5 +2,6 @@ import axios from 'axios';
 
 
 export default axios.create({
-    baseURL : 'http://localhost:5000',
+   // baseURL : 'http://localhost:5000',
+    baseURL : 'https://backend-sistema-escolar.vercel.app/',
 })
